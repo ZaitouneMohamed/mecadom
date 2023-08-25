2fe7d68f158dbccc23bfb01520553d28e92d401e
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-26 00:23:08 +0100

    add spatie roles and pernimmsions

--- config/app.php
+++ config/app.php
@@ -153,6 +153,7 @@ return [
         Illuminate\Foundation\Providers\FoundationServiceProvider::class,
         Illuminate\Hashing\HashServiceProvider::class,
         Illuminate\Mail\MailServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
         Illuminate\Notifications\NotificationServiceProvider::class,
         Illuminate\Pagination\PaginationServiceProvider::class,
         Illuminate\Pipeline\PipelineServiceProvider::class,
