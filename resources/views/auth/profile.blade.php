4bb73368f20193f4f18c248bf4a1610066e27adb
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-26 16:42:00 +0100

    Update profile.blade.php

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -6,31 +6,68 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/general.css') }}">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+
+    <style>
+        .nav-btn .mobile-btn {
+            display: none;
+        }
+
+        @media (max-width: 640px) {
+            .nmenu .nav-btn {
+                display: none;
+            }
+        }
+    </style>
+
 </head>
 
-<body>
-    <table class="table-auto">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>name</th>
-                <th>email</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach (\App\Models\User::latest()->get() as $item)
-                <tr>
-                    <td>{{ $item->id }}</td>
-                    <td>{{ $item->first_name }} {{ $item->last_name }}</td>
-                    <td>{{$item->email}}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <main class="flex items-center justify-center min-h-screen">
-        <h1 class="text-5xl">Profile {{ auth()->user()->first_name }} {{ auth()->user()->last_name }}</h1>
-        <a href="{{ route('logout') }}">Log Out</a><br><br>
+<body class="p-1">
+    <header>
+        <nav>
+            <a href="https://meca-dom-plus.vercel.app/" class="logo">
+                <img src="https://raw.githubusercontent.com/jealife/Login-form-Mecadom/main/images/Logotype_Mecadom_png_Fichier%203.png"
+                    alt=" Logo MECADOM" />
+            </a>
+            <button onclick="showMenu()" id="hamburger_menu" class="button_menu">
+                <i id="bxBtnmenu" class="bx bx-menu"></i>
+                <i id="bxBtnx" style="display: none" class="bx bx-x"></i>
+            </button>
+            <div class="menu">
+                <div class="links-menu">
+                    <a href="https://mecadom.electroniqueclasse.com/" class="link-menu">Accueil</a>
+                    <a href="#" class="link-menu">Services</a>
+                    <a href="#" class="link-menu">Produits</a>
+                    <a href="#" class="link-menu">Fovoris</a>
+                </div>
+                <div class="nav-btn  ">
+                    <a href="#" class="login hidden">Log out</a>
+                    <a href="./pages/profile/profile.html" class="register hidden">Profile</a>
+                </div>
+            </div>
+            <div class="nav-btn mobile-btn  ">
+                <a href="#" class="login hidden">Log out</a>
+                <a href="./pages/profile/profile.html" class="register hidden">Profile</a>
+            </div>
+        </nav>
+    </header>
+    <main class="flex flex-col  justify-center min-h-screen py-14">
+
+
+        <div class="hero flex items-center justify-center min-h-screen">
+
+            <h1 class="text-2xl">Bienvenue, </h1>
+        </div>
+
+        <div class="utilisateurs">
+
+        </div>
     </main>
+
+    <script src="{{ asset('assets/js/nav.js') }} "></script>
 </body>
+
 
 </html>
