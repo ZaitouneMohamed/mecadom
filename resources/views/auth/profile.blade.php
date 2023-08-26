dd08c0b12791c1030e213f5418910ea3d2a20607
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-08-26 15:34:10 +0100

    Update profile.blade.php

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -9,10 +9,28 @@
 </head>
 
 <body>
+    <table class="table-auto">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>name</th>
+                <th>email</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach (\App\Models\User::latest()->get() as $item)
+                <tr>
+                    <td>{{ $item->id }}</td>
+                    <td>{{ $item->first_name }} {{ $item->last_name }}</td>
+                    <td>{{$item->email}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
     <main class="flex items-center justify-center min-h-screen">
         <h1 class="text-5xl">Profile</h1>
         <h1 class="text-5xl">hello mr : {{ auth()->user()->first_name }} {{ auth()->user()->last_name }} </h1>
-        <a href="{{ route('logout') }}">Log Out</a>
+        <a href="{{ route('logout') }}">Log Out</a><br><br>
     </main>
 </body>
 
