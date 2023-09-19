db3976585ba25ba1e661533e8d3bfa0d979a7009
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-09-19 19:39:05 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -3,6 +3,14 @@
 use Illuminate\Support\Facades\Facade;
 
 return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
 
     /*
     |--------------------------------------------------------------------------
