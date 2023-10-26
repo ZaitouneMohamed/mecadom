fd58efcbf98be5c0e5a814d280ae1ad2380d1e29
mohamed zaitoune <115373788+ZaitouneMohamed@users.noreply.github.com>
2023-10-26 12:20:19 +0100

    Update services.blade.php

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -107,28 +107,28 @@
 
         <!-- <div class="w-full flex flex-col items-center justify-centerpx-24 px-5 relative -mt-5 lg:-mt-10">
 
-                                    <div class="search-page-form w-full  max-w-[1440px] relative mb-4 absolute z-[999] ">
-                                        <form>
-                                            <div class="flex lg:items-center lg:justify-center w-full">
-                                                <div class="relative lg:w-96 w-full ">
-                                                    <input type="search" id="search-dropdown" style="border-radius: 12px; padding: 1rem ;"
-                                                        class="block p-2.5 py-5 w-full z-20 text-sm text-gray-900 bg-gray-50  border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500   bg-blue-500 shadow-lg "
-                                                        placeholder="Dépannage, Lavage, Batterie, Change Pneu..." required>
-                                                    <button type="submit"
-                                                        class="absolute top-0 right-0  text-sm font-medium h-full text-white bg-blue-700 rounded-md px-5  border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                                        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                            viewBox="0 0 20 20">
-                                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                                stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                                        </svg>
-                                                        <span class="sr-only">Rechercher</span>
-                                                    </button>
+                                                <div class="search-page-form w-full  max-w-[1440px] relative mb-4 absolute z-[999] ">
+                                                    <form>
+                                                        <div class="flex lg:items-center lg:justify-center w-full">
+                                                            <div class="relative lg:w-96 w-full ">
+                                                                <input type="search" id="search-dropdown" style="border-radius: 12px; padding: 1rem ;"
+                                                                    class="block p-2.5 py-5 w-full z-20 text-sm text-gray-900 bg-gray-50  border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500   bg-blue-500 shadow-lg "
+                                                                    placeholder="Dépannage, Lavage, Batterie, Change Pneu..." required>
+                                                                <button type="submit"
+                                                                    class="absolute top-0 right-0  text-sm font-medium h-full text-white bg-blue-700 rounded-md px-5  border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                                                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                                        viewBox="0 0 20 20">
+                                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                                            stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                                                    </svg>
+                                                                    <span class="sr-only">Rechercher</span>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </form>
                                                 </div>
-                                            </div>
-                                        </form>
-                                    </div>
 
-                                </div> -->
+                                            </div> -->
 
 
         <div class="w-full flex flex-col items-center justify-center px-4  mt-4">
@@ -196,7 +196,7 @@
                             </div>
                             <div
                                 class="card-profile-wrapper w-full flex  justify-between items-center px-4 self-center my-4">
-                                <a href="../profile-prestataire/prestataire.html" class="user flex gap-2">
+                                <a href="{{ route('prestateur.profile', $user->id) }}" class="user flex gap-2">
                                     <div class="flex flex-col">
                                         <h3 class="text-slate-950 font-bold hover:text-[#ffc107] duration-300">
                                             {{ $user->full_name }}</h3>
@@ -219,7 +219,8 @@
                             <div>
                                 <div class="service px-4 border-t py-4 flex flex-wrap gap-2">
                                     @forelse ($user->Services as $service)
-                                        {{ $service->name }}
+                                        <span
+                                            class="call text-slate-500 text-xs p-2 rounded-full border-gray-100 border">{{ $service->name }}</span>
                                     @empty
                                     @endforelse
                                 </div>
@@ -238,18 +239,18 @@
         <section class="px-2 lg:px-4 w-full flex flex-col gap-10 items-center justify-center py-5 px-5 min-h-max ">
 
             <!-- <button disabled="" type="button"
-                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center">
-                                        <svg aria-hidden="true" role="status" class="inline mr-3 w-4 h-4 text-white animate-spin"
-                                            viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path
-                                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                                fill="#E5E7EB"></path>
-                                            <path
-                                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                                fill="currentColor"></path>
-                                        </svg>
-                                        Loading...
-                                    </button> -->
+                                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center">
+                                                    <svg aria-hidden="true" role="status" class="inline mr-3 w-4 h-4 text-white animate-spin"
+                                                        viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path
+                                                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                            fill="#E5E7EB"></path>
+                                                        <path
+                                                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                            fill="currentColor"></path>
+                                                    </svg>
+                                                    Loading...
+                                                </button> -->
             <button disabled="" type="button"
                 class="py-2.5 px-5 mr-2 text-sm font-medium text-gray-900 bg-white rounded border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center">
                 <svg aria-hidden="true" role="status"
